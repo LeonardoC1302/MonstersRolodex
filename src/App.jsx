5b71d2f594a4d05cc2da1a19d6c1dfaba68c97a4
Leonardo Céspedes <lecespedes@estudiantec.cr>
2024-02-26 10:08:09 -0600

    Components: Initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react';
+
+import CardList from './components/card-list/card-list.component';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+      () => {
+        console.log(this.state);
+      }));
+  }
+
+  onSearchChange = (event) =>{
+    const searchField = event.target.value.toLowerCase()
+    this.setState(() => {return{ searchField }});
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {return monster.name.toLowerCase().includes(searchField)});
+    return (
+      <div className='App'>
+      <input className='search-box' type='search' placeholder='Search Monsters' onChange={onSearchChange}/>
+        {
+          // filteredMonsters.map((monster) => {
+          //   return (
+          //     <div key={monster.id}>
+          //       <h1>{monster.name}</h1>
+          //     </div>
+          //   )
+          // })
+          <CardList />
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
